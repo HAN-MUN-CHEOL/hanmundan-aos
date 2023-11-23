b8e6d0409a1df4c637aae36f1688366b8df854e0
m1nz00 <ssssssun56@sookmyung.ac.kr>
2023-11-23 15:40:53 +0900

    #3 회원가입 xml 수정 activity 추가

---
+++ app/src/main/java/com/sookmyung/hanmundan/ui/join/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.sookmyung.hanmundan.ui.join
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sookmyung.hanmundan.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
