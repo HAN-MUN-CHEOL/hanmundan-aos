7884513f2e1ffe4035e0e1c10d039f444c7a7d49
m1nz00 <ssssssun56@sookmyung.ac.kr>
2023-11-27 23:36:18 +0900

    #9 메인 xml 수정 네비게이션 xml 연결

--- app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.sookmyung.hanmundan.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.sookmyung.hanmundan.R
+import com.sookmyung.hanmundan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val headerNavigation = binding.nvMenu.getHeaderView(0)
+        val btnMenuClose = headerNavigation.findViewById<ImageView>(R.id.iv_navigation_navi)
+
+        binding.ivMainNavi.setOnClickListener {
+            binding.dlMain.openDrawer(GravityCompat.END)
+        }
+
+        btnMenuClose.setOnClickListener {
+            binding.dlMain.closeDrawers()
+        }
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_main -> {
+                return true
+            }
+
+            R.id.menu_item_calendar -> {
+                return true
+            }
+
+            R.id.menu_item_bookmark -> {
+                return true
+            }
+
+            R.id.menu_item_my_page -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (binding.dlMain.isDrawerOpen(GravityCompat.END)) {
+                binding.dlMain.closeDrawers()
+            } else {
+                finish()
+            }
+        }
     }
 }
