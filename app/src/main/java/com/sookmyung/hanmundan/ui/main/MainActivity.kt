2d4ab5093cd3fc3f33439e7d6e7f21eb3b3a134b
m1nz00 <ssssssun56@sookmyung.ac.kr>
2023-11-30 14:43:35 +0900

    #12 메인, 달력 액티비티 연결 회원가입 이미지 id 추가

--- app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.sookmyung.hanmundan.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.sookmyung.hanmundan.R
 import com.sookmyung.hanmundan.databinding.ActivityMainBinding
+import com.sookmyung.hanmundan.ui.calender.CalenderActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -17,8 +20,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val headerNavigation = binding.nvMenu.getHeaderView(0)
+        val headerNavigation = binding.nvMainMenu.getHeaderView(0)
         val btnMenuClose = headerNavigation.findViewById<ImageView>(R.id.iv_navigation_navi)
+        val navigationView = binding.nvMainMenu
+
+        navigationView.setNavigationItemSelectedListener(this)
 
         binding.ivMainNavi.setOnClickListener {
             binding.dlMain.openDrawer(GravityCompat.END)
@@ -31,14 +37,16 @@ class MainActivity : AppCompatActivity() {
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_item_main -> {
-                return true
+                binding.dlMain.closeDrawers()
             }
 
             R.id.menu_item_calendar -> {
-                return true
+                val intentToCalender = Intent(this, CalenderActivity::class.java)
+                startActivity(intentToCalender)
+                binding.dlMain.closeDrawers()
             }
 
             R.id.menu_item_bookmark -> {
@@ -49,7 +57,7 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         }
-        return super.onOptionsItemSelected(item)
+        return false
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
