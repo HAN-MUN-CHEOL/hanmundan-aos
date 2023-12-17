9478804f1cf567f630f62aa90bf2bf2bed69190e
m1nz00 <ssssssun56@sookmyung.ac.kr>
2023-12-17 19:23:40 +0900

    #29 저장하기 기능 작성

--- app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/main/MainActivity.kt
@@ -16,12 +16,15 @@ import com.sookmyung.hanmundan.ui.bookmark.BookmarkActivity
 import com.sookmyung.hanmundan.ui.calender.CalenderActivity
 import com.sookmyung.hanmundan.ui.myPage.MyPageActivity
 import com.sookmyung.hanmundan.util.SnackbarCustom
-import com.sookmyung.hanmundan.util.hideKeyboard
+import com.sookmyung.hanmundan.util.toast
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    var saveWritingState = false
+    var lastWriting: String? = null
+    var currentWriting: String? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,36 +39,25 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         var bookmarkState = false
         var moreMeaningState = false
 
-        binding.ivMainBlankedBookmark.setOnClickListener {
-            if (!bookmarkState) {
-                binding.ivMainBlankedBookmark.setImageResource(R.drawable.ic_bookmark_fill)
-                SnackbarCustom.make(binding.root, "책갈피를 끼웠습니다.").show()
-                bookmarkState = true
-            } else {
-                binding.ivMainBlankedBookmark.setImageResource(R.drawable.ic_bookmark_blank)
-                SnackbarCustom.make(binding.root, "책갈피를 뺐습니다.").show()
-                bookmarkState = false
-            }
-        }
-
-        binding.tvMainMoreMeaning.setOnClickListener {
-            if (!moreMeaningState) {
-                binding.tvMainWordMeaning.text =
-                    "1. 인류 사회의 변천과 흥망의 과정. 또는 그 기록.\n\n2. 어떠한 사물이나 사실이 존재해 온 연혁.\n\n3. 자연 현상이 변하여 온 자취.\n\n4. 역으로 쓰는 건물."
-                binding.tvMainMoreMeaning.text = "접기"
-                moreMeaningState = true
-            } else {
-                binding.tvMainWordMeaning.text =
-                    "1. 인류 사회의 변천과 흥망의 과정. 또는 그 기록.\n\n2. 어떠한 사물이나 사실이 존재해 온 연혁."
-                binding.tvMainMoreMeaning.text = "더 보기"
-                moreMeaningState = false
-            }
-        }
-
+        initClick(bookmarkState, moreMeaningState)
         navigationView.setNavigationItemSelectedListener(this)
+        initMenuNickname(textMenuNickname, nickname)
+        initDrawer(btnMenuClose)
 
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    private fun initClick(bookmarkState: Boolean, moreMeaningState: Boolean) {
+        clickBookmarkButton(bookmarkState)
+        clickMoreMeaningButton(moreMeaningState)
+        clickSaveButton()
+    }
+
+    private fun initMenuNickname(textMenuNickname: TextView, nickname: String?) {
         textMenuNickname.text = "$nickname 님"
+    }
 
+    private fun initDrawer(btnMenuClose: ImageView) {
         binding.ivMainNavi.setOnClickListener {
             binding.dlMain.openDrawer(GravityCompat.END)
         }
@@ -73,12 +65,64 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         btnMenuClose.setOnClickListener {
             binding.dlMain.closeDrawers()
         }
+    }
 
-        binding.root.setOnClickListener { view ->
-            hideKeyboard(view)
+    private fun clickMoreMeaningButton(moreMeaningState: Boolean) {
+        var moreMeaningState1 = moreMeaningState
+        binding.tvMainMoreMeaning.setOnClickListener {
+            if (!moreMeaningState1) {
+                binding.tvMainWordMeaning.text =
+                    "1. 인류 사회의 변천과 흥망의 과정. 또는 그 기록.\n\n2. 어떠한 사물이나 사실이 존재해 온 연혁.\n\n3. 자연 현상이 변하여 온 자취.\n\n4. 역으로 쓰는 건물."
+                binding.tvMainMoreMeaning.text = "접기"
+                moreMeaningState1 = true
+            } else {
+                binding.tvMainWordMeaning.text =
+                    "1. 인류 사회의 변천과 흥망의 과정. 또는 그 기록.\n\n2. 어떠한 사물이나 사실이 존재해 온 연혁."
+                binding.tvMainMoreMeaning.text = "더 보기"
+                moreMeaningState1 = false
+            }
         }
+    }
 
-        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    private fun clickBookmarkButton(bookmarkState: Boolean) {
+        var bookmarkState1 = bookmarkState
+        binding.ivMainBlankedBookmark.setOnClickListener {
+            if (!bookmarkState1) {
+                binding.ivMainBlankedBookmark.setImageResource(R.drawable.ic_bookmark_fill)
+                SnackbarCustom.make(binding.root, "책갈피를 끼웠습니다.").show()
+                bookmarkState1 = true
+            } else {
+                binding.ivMainBlankedBookmark.setImageResource(R.drawable.ic_bookmark_blank)
+                SnackbarCustom.make(binding.root, "책갈피를 뺐습니다.").show()
+                bookmarkState1 = false
+            }
+        }
+    }
+
+    private fun clickSaveButton() {
+        binding.tvMainSaveWriting.setOnClickListener {
+            if (lastWriting == null) {
+                lastWriting = binding.etMainWriting.text.toString()
+                currentWriting = binding.etMainWriting.text.toString()
+            } else {
+                lastWriting = currentWriting
+                currentWriting = binding.etMainWriting.text.toString()
+            }
+            updateSaveWritingState()
+            if (saveWritingState) {
+                SnackbarCustom.make(binding.root, "저장되었습니다.").show()
+            } else {
+                SnackbarCustom.make(binding.root, "저장되지 않았습니다.").show()
+            }
+        }
+    }
+
+    fun updateSaveWritingState() {
+        saveWritingState = if (lastWriting == null) {
+            false
+        } else {
+            lastWriting == currentWriting
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -112,7 +156,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         override fun handleOnBackPressed() {
             if (binding.dlMain.isDrawerOpen(GravityCompat.END)) {
                 binding.dlMain.closeDrawers()
-            } else {
+            }
+            if (!saveWritingState) {
+                toast("저장안함")
+                // Dialog
+            }
+            else {
                 finish()
             }
         }
