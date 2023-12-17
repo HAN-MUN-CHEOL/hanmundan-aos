0142e4ae2eb2554e36a744147dfbb1c413fbb727
m1nz00 <ssssssun56@sookmyung.ac.kr>
2023-12-17 17:08:33 +0900

    #27 닉네임 전달

--- app/src/main/java/com/sookmyung/hanmundan/ui/joinSuccess/JoinSuccessActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/joinSuccess/JoinSuccessActivity.kt
@@ -1,21 +1,28 @@
 package com.sookmyung.hanmundan.ui.joinSuccess
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
 import com.sookmyung.hanmundan.R
+import com.sookmyung.hanmundan.databinding.ActivityJoinSuccessBinding
 import com.sookmyung.hanmundan.ui.main.MainActivity
+import com.sookmyung.hanmundan.util.binding.BindingActivity
 
 
-class JoinSuccessActivity : AppCompatActivity() {
+class JoinSuccessActivity : BindingActivity<ActivityJoinSuccessBinding>(R.layout.activity_join_success) {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_success)
+        val intentFromJoin = intent
+        val nickname = intentFromJoin.getStringExtra("nickname")
+
+        binding.tvJoinSuccessNickname.text = "$nickname 님"
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intentToMain = Intent(this, MainActivity::class.java)
+            intentToMain.putExtra("nickname", nickname)
             startActivity(intentToMain)
             finish()
         }, 1000)
