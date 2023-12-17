529214fd9965eaa19e9678f54676976ff1ea2e77
river99 <qkrrkd42@naver.com>
2023-12-17 23:25:40 +0900

    #32[ui] 네비게이션 메뉴 화면 전환

--- app/src/main/java/com/sookmyung/hanmundan/ui/myPage/MyPageActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/myPage/MyPageActivity.kt
@@ -3,26 +3,59 @@ package com.sookmyung.hanmundan.ui.myPage
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.Window
+import android.widget.ImageView
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textview.MaterialTextView
 import com.sookmyung.hanmundan.R
 import com.sookmyung.hanmundan.databinding.ActivityMyPageBinding
+import com.sookmyung.hanmundan.ui.bookmark.BookmarkActivity
+import com.sookmyung.hanmundan.ui.calender.CalenderActivity
+import com.sookmyung.hanmundan.ui.main.MainActivity
 import com.sookmyung.hanmundan.util.binding.BindingActivity
+import com.sookmyung.hanmundan.util.hideKeyboard
 
-class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
+class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page),
+    NavigationView.OnNavigationItemSelectedListener {
     private lateinit var resignDialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initClickListener()
+        val headerNavigation =
+            binding.nvMenu.getHeaderView(0).findViewById<ImageView>(R.id.iv_navigation_navi)
+        val btnMenuClose = headerNavigation.findViewById<ImageView>(R.id.iv_navigation_navi)
+
+        initNavi(btnMenuClose)
+    }
+
+    private fun initNavi(btnMenuClose: ImageView) {
         resignDialog = Dialog(this)
         resignDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         resignDialog.setContentView(R.layout.dialog_frame)
+
+        binding.nvMenu.setNavigationItemSelectedListener(this)
+
+        binding.ivMyPageNavi.setOnClickListener {
+            binding.dlMyPage.openDrawer(GravityCompat.END)
+        }
+        btnMenuClose.setOnClickListener {
+            binding.dlMyPage.closeDrawers()
+        }
     }
 
-    private fun initClickListener(){
+    private fun initClickListener() {
         initTextClickListener()
-        initchangeNicknameClickListener()
+        initChangeNicknameClickListener()
         initResignClickListener()
+        initHideKeyBoard()
+    }
+
+    private fun initHideKeyBoard() {
+        binding.root.setOnClickListener { view ->
+            hideKeyboard(view)
+        }
     }
 
     private fun initResignClickListener() {
@@ -32,13 +65,13 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
     }
 
     private fun initTextClickListener() {
-        binding.tvMyPageMenuPasswordChange.setOnClickListener{
+        binding.tvMyPageMenuPasswordChange.setOnClickListener {
             val intentToPasswordChange = Intent(this, ChangePasswordActivity::class.java)
             startActivity(intentToPasswordChange)
         }
     }
 
-    private fun initchangeNicknameClickListener() {
+    private fun initChangeNicknameClickListener() {
         binding.tvMyPageMenuNicknameChange.setOnClickListener {
             val intentToChangeNickname = Intent(this, ChangeNickNameActivity::class.java)
             startActivity(intentToChangeNickname)
@@ -55,4 +88,35 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
         resignDialog.findViewById<MaterialTextView>(R.id.btn_dialog_yes)
             .setOnClickListener { finish() }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_main -> {
+                val intentToMain = Intent(this, MainActivity::class.java)
+                startActivity(intentToMain)
+                finish()
+                binding.dlMyPage.closeDrawers()
+            }
+
+            R.id.menu_item_calendar -> {
+                val intentToCalender = Intent(this, CalenderActivity::class.java)
+                startActivity(intentToCalender)
+                finish()
+                binding.dlMyPage.closeDrawers()
+            }
+
+            R.id.menu_item_bookmark -> {
+                val intentToBookmark = Intent(this, BookmarkActivity::class.java)
+                startActivity(intentToBookmark)
+                finish()
+                binding.dlMyPage.closeDrawers()
+            }
+
+            R.id.menu_item_my_page -> {
+                binding.dlMyPage.closeDrawers()
+            }
+        }
+        return false
+    }
+
 }
