bc8f0a15ff7d5640bcb8382d7757be20464ceae5
river99 <qkrrkd42@naver.com>
2023-12-17 21:17:56 +0900

    commit

---
+++ app/src/main/java/com/sookmyung/hanmundan/ui/myPage/ChangePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.sookmyung.hanmundan.ui.myPage
+
+import android.os.Bundle
+import com.sookmyung.hanmundan.R
+import com.sookmyung.hanmundan.databinding.ActivityChangePasswordBinding
+import com.sookmyung.hanmundan.util.binding.BindingActivity
+
+class ChangePasswordActivity :
+    BindingActivity<ActivityChangePasswordBinding>(R.layout.activity_change_password) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
