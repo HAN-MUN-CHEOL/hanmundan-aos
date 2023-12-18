cfb22ba71a9550ec837f532ac7926f0fd42db9d2
river99 <qkrrkd42@naver.com>
2023-12-18 18:31:11 +0900

    #47 파이어베이스 연동해서 쓴 글 개수 표시하기

--- app/src/main/java/com/sookmyung/hanmundan/ui/myPage/MyPageActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/myPage/MyPageActivity.kt
@@ -6,13 +6,19 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.GravityCompat
+import com.google.android.gms.tasks.Task
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 import com.sookmyung.hanmundan.R
 import com.sookmyung.hanmundan.databinding.ActivityMyPageBinding
 import com.sookmyung.hanmundan.ui.bookmark.BookmarkActivity
@@ -20,10 +26,21 @@ import com.sookmyung.hanmundan.ui.calender.CalenderActivity
 import com.sookmyung.hanmundan.ui.main.MainActivity
 import com.sookmyung.hanmundan.util.binding.BindingActivity
 import com.sookmyung.hanmundan.util.hideKeyboard
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page),
     NavigationView.OnNavigationItemSelectedListener {
     private lateinit var resignDialog: Dialog
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var databaseReal: DatabaseReference
+    private lateinit var sentenceInDB: String
+    private var featherCount = 0;
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +50,50 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
         val navigationView = binding.nvMenu
         val textMenuNickname =
             headerNavigation.findViewById<TextView>(R.id.tv_navigation_header_name)
-        val spf: SharedPreferences = applicationContext.getSharedPreferences("user", Context.MODE_PRIVATE)
-        val nickname = spf.getString("nickname","")
+        val spf: SharedPreferences =
+            applicationContext.getSharedPreferences("user", Context.MODE_PRIVATE)
+        val nickname = spf.getString("nickname", "")
         binding.tvMyPageNickname.text = "${nickname}님"
         initDialog()
         navigationView.setNavigationItemSelectedListener(this)
         initMenuNickname(textMenuNickname, nickname)
         initNavi(btnMenuClose)
+
+        FirebaseApp.initializeApp(this)
+        databaseReal =
+            Firebase.database("https://hanmundan-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+
+        coroutineScope.launch {
+            countFeather()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private suspend fun countFeather() {
+        try {
+            val databaseReference = databaseReal.child("hanmundan")
+            val dataSnapshot = databaseReference.get().await()
+            for (childSnapshot in dataSnapshot.children) {
+                sentenceInDB =
+                    childSnapshot.child("sentence").getValue(String::class.java).toString()
+                if (sentenceInDB.isNullOrBlank()) continue
+                else featherCount++
+            }
+            binding.tvMyPageFeatherCount.text = "${featherCount}개"
+        } catch (exception: Exception) {
+            Log.d("hmm", "Error getting data: ", exception)
+        }
+    }
+
+    private suspend fun <T> Task<T>.await(): T {
+        return suspendCancellableCoroutine { continuation ->
+            addOnSuccessListener { result ->
+                continuation.resume(result)
+            }
+            addOnFailureListener { exception ->
+                continuation.resumeWithException(exception)
+            }
+        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -50,8 +104,9 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
     @SuppressLint("SetTextI18n")
     override fun onResume() {
         super.onResume()
-        val spf: SharedPreferences = applicationContext.getSharedPreferences("user", Context.MODE_PRIVATE)
-        val nickname = spf.getString("nickname","")
+        val spf: SharedPreferences =
+            applicationContext.getSharedPreferences("user", Context.MODE_PRIVATE)
+        val nickname = spf.getString("nickname", "")
         val headerNavigation = binding.nvMenu.getHeaderView(0)
         val textMenuNickname =
             headerNavigation.findViewById<TextView>(R.id.tv_navigation_header_name)
