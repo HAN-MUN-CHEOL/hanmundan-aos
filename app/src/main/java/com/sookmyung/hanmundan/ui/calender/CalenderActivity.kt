1322742d7a866494898710213c5f8717c267e64f
river99 <qkrrkd42@naver.com>
2023-12-18 18:03:25 +0900

    #44 파이어베이스 연동해서 calender에 단어 띄우고, 퇴고하기로 data 넘기기

--- app/src/main/java/com/sookmyung/hanmundan/ui/calender/CalenderActivity.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/calender/CalenderActivity.kt
@@ -5,27 +5,53 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.core.view.GravityCompat
+import com.google.android.gms.tasks.Task
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 import com.sookmyung.hanmundan.R
 import com.sookmyung.hanmundan.databinding.ActivityCalenderBinding
+import com.sookmyung.hanmundan.model.DailyRecord
 import com.sookmyung.hanmundan.ui.bookmark.BookmarkActivity
 import com.sookmyung.hanmundan.ui.main.MainActivity
 import com.sookmyung.hanmundan.ui.myPage.MyPageActivity
 import com.sookmyung.hanmundan.ui.rewrite.RewriteActivity
+import com.sookmyung.hanmundan.util.SnackbarCustom
 import com.sookmyung.hanmundan.util.binding.BindingActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.properties.Delegates
 
 class CalenderActivity : BindingActivity<ActivityCalenderBinding>(R.layout.activity_calender),
     NavigationView.OnNavigationItemSelectedListener {
     private val viewModel: CalenderViewModel by viewModels()
     private var date by Delegates.notNull<Long>()
+    private var realDate: String = getCurrentDate()
+
+    private lateinit var databaseReal: DatabaseReference
+    private lateinit var dateInDatabase: String
+    private lateinit var todayDocumentId: String
+    private lateinit var todayWord: String
+    private var todayWordSentence: String? = null
+    private var todayWordBookmarkState: Boolean = false
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var bookmarkState = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
@@ -43,6 +69,106 @@ class CalenderActivity : BindingActivity<ActivityCalenderBinding>(R.layout.activ
         initMenuNickname(textMenuNickname, nickname)
         initNavi(btnMenuClose)
         initClickListener()
+
+        FirebaseApp.initializeApp(this)
+        databaseReal =
+            Firebase.database("https://hanmundan-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
+
+        coroutineScope.launch {
+            initWord()
+            initUI()
+            initClick()
+        }
+    }
+
+    private fun initUI() {
+        binding.tvCalenderWordTitle.text = todayWord
+        binding.tvCalenderSentence.text =
+            Editable.Factory.getInstance().newEditable(todayWordSentence)
+        bookmarkState = todayWordBookmarkState
+    }
+
+    private fun initClick() {
+        clickBookmarkButton()
+    }
+
+    private fun clickBookmarkButton() {
+        binding.ivCalenderBookmark.setOnClickListener {
+            bookmarkState = !bookmarkState
+            if (bookmarkState) {
+                binding.ivCalenderBookmark.setImageResource(R.drawable.ic_bookmark_fill)
+                SnackbarCustom.make(binding.root, "책갈피를 끼웠습니다.").show()
+            } else {
+                binding.ivCalenderBookmark.setImageResource(R.drawable.ic_bookmark_blank)
+                SnackbarCustom.make(binding.root, "책갈피를 뺐습니다.").show()
+            }
+            setDocument(
+                DailyRecord(
+                    todayWord,
+                    todayWordSentence ?: "",
+                    dateInDatabase,
+                    bookmarkState
+                )
+            )
+        }
+    }
+
+    private fun setDocument(data: DailyRecord) {
+        databaseReal.child("hanmundan").child(todayDocumentId).setValue(data)
+            .addOnSuccessListener {
+                Log.e("hmm", "setDocument success")
+            }
+            .addOnFailureListener {
+                Log.e("hmm", "setDocument fail")
+            }
+    }
+
+    private suspend fun initWord() {
+        try {
+            val databaseReference = databaseReal.child("hanmundan")
+            val dataSnapshot = databaseReference.get().await()
+            for (childSnapshot in dataSnapshot.children) {
+                dateInDatabase = childSnapshot.child("date").getValue(String::class.java).toString()
+                if (dateInDatabase == realDate) {
+                    todayDocumentId = childSnapshot.key!!
+                    todayWord = childSnapshot.child("word").getValue(String::class.java)!!
+                    todayWordSentence =
+                        childSnapshot.child("sentence").getValue(String::class.java) ?: ""
+                    bookmarkState =
+                        childSnapshot.child("bookmark").getValue(Boolean::class.java) ?: false
+                    viewModel.updateDailyRecord(
+                        DailyRecord(
+                            todayWord,
+                            todayWordSentence ?: "",
+                            realDate,
+                            bookmarkState
+                        )
+                    )
+                    break
+                }
+            }
+        } catch (exception: Exception) {
+            Log.d("hmm", "Error getting data: ", exception)
+        }
+    }
+
+    private fun getCurrentDate(): String {
+        val currentDate = System.currentTimeMillis()
+        val date = Date(currentDate)
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd").format(date)
+        val todayDate = dateFormat.format(date)
+        return todayDate
+    }
+
+    private suspend fun <T> Task<T>.await(): T {
+        return suspendCancellableCoroutine { continuation ->
+            addOnSuccessListener { result ->
+                continuation.resume(result)
+            }
+            addOnFailureListener { exception ->
+                continuation.resumeWithException(exception)
+            }
+        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -53,6 +179,10 @@ class CalenderActivity : BindingActivity<ActivityCalenderBinding>(R.layout.activ
     private fun initClickListener() {
         binding.btnCalenderRetirement.setOnClickListener {
             val intentToRetirement = Intent(this, RewriteActivity::class.java)
+            intentToRetirement.putExtra("word", todayWord)
+            intentToRetirement.putExtra("sentence", todayWordSentence)
+            intentToRetirement.putExtra("date", realDate)
+            intentToRetirement.putExtra("bookmark", todayWordBookmarkState)
             startActivity(intentToRetirement)
         }
         binding.ivCalenderBookmark.setOnClickListener { viewModel.updateBookmark() }
@@ -80,7 +210,11 @@ class CalenderActivity : BindingActivity<ActivityCalenderBinding>(R.layout.activ
 
             if (selectedDate.timeInMillis != date) {
                 date = selectedDate.timeInMillis
+                realDate = "${year}.${month + 1}.${dayOfMonth}"
                 Log.e("kang", "$year $month $dayOfMonth")
+            }
+            coroutineScope.launch {
+                initWord()
             }
         }
     }
--- app/src/main/java/com/sookmyung/hanmundan/ui/calender/CalenderViewModel.kt
+++ app/src/main/java/com/sookmyung/hanmundan/ui/calender/CalenderViewModel.kt
@@ -7,15 +7,15 @@ import com.sookmyung.hanmundan.model.DailyRecord
 
 class CalenderViewModel : ViewModel() {
     private val _dailyRecord =
-        MutableLiveData<DailyRecord>(DailyRecord("실험", "이 데이터는 순전히 실험용입니다.", "2023.10.12", false))
+        MutableLiveData<DailyRecord>()
     val dailyRecord: LiveData<DailyRecord> = _dailyRecord
-
-    private fun getDailyRecord() {
-        //TODO(날짜에 맞는 DailyRecord 가져올 것)
-    }
 
     fun updateBookmark(){
         val dr = _dailyRecord.value?.copy(bookmark = !(_dailyRecord.value!!.bookmark))
         _dailyRecord.value = dr!!
+    }
+
+    fun updateDailyRecord(dr: DailyRecord){
+        _dailyRecord.value = dr
     }
 }
